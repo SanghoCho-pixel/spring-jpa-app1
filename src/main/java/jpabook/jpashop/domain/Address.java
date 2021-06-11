6fbfc04985b1da25f78aff179df172bff242bdd5
SanghoCho <shxho0476@gmail.com>
2021-06-11 22:25:53 +0900

    feat:주문 도메인 개발 및 테스트

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -1,8 +1,13 @@
 package jpabook.jpashop.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     private String city;
